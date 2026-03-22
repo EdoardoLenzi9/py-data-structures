39731794e8b51d546a7c173c6311d6339927159f
edoardolenzi9@gmail.com <edoardolenzi9@gmail.com>
2026-03-22 16:59:52 +0100

    wip

---
+++ exercises/02_list_comprehension.py
@@ -0,0 +1,38 @@
+# 01. inizializza una lista con i numeri da 1 a 100 usando un ciclo for
+
+...
+
+# 02. inizializza una lista con i numeri da 1 a 100 usando un ciclo while
+
+...
+
+# 03. inizializza una lista con i numeri da 1 a 100 usando range e ciclo for
+
+...
+
+# 04. inizializza una lista con i numeri da 1 a 100 usando range ebbbbasta
+
+...
+
+# 05. data una lista con i numeri da 1 a 100 stampala
+
+...
+
+# 06. data una lista `x` con i numeri da 1 a 100 stampa una lista con tutti i numeri raddoppiati (2,4,6,8...)
+
+x = ...
+
+# 07. rifai l'esercizio 06 ma usa la list comprehension per raddoppiare i numeri e generare la lista y poi stampala
+
+x = ...
+y = ...
+print(y)
+
+# 08. usa la list comprehension su y per eliminare dalla lista tutti i multipli di 10, salva in z e stampala
+
+x = ...
+y = ...
+z = ...
+print(z)
+
+# 09. 
