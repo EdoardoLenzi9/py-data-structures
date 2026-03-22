1722abb17271f86bd0d1556a7d701f1cef4d3992
edoardolenzi9@gmail.com <edoardolenzi9@gmail.com>
2026-03-22 15:15:32 +0100

    wip

--- exercises/01_list.py
+++ exercises/01_list.py
@@ -4,4 +4,12 @@ response = "una sequenza e' ..."
 # 02. cos'e' una lista/array?
 response = "una lista e' ..."
 
-# 03. 
+# 03. qual'e' la differenza fra un'array di caratteri e una stringa?
+# hint: mutable vs immutable
+response = "la differenza ..."
+
+# 04. quanto spazio in memoria RAM occupa una stringa?
+# [ ] A. 1Byte
+# [ ] B. 1Byte per ogni carattere
+# [ ] C. 1KiloByte per ogni carattere
+
