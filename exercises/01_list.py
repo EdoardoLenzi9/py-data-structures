7383d42ea60b780c884708be4e82e616690b866e
edoardolenzi9@gmail.com <edoardolenzi9@gmail.com>
2026-03-22 15:45:39 +0100

    wip

--- exercises/01_list.py
+++ exercises/01_list.py
@@ -53,4 +53,19 @@ y
 y.append("b")
 y
 
-
+# 10. operazioni sulle liste
+# - inizializza una lista di stringhe vuota x
+# - misura lo spazio in memoria occupato dal valore in x
+# - leggi l'identificativo dello spazio in memoria occupato dal valore di x
+# - sovrascrivi x con una nuova lista contentente una solo valore "a"
+# - controlla l'id di x (dovrebbe essere cambiato)
+# - aggiungi ad x un nuovo valore "b" (senza reinizializzare la lista)
+# - modifica l'ultimo valore in x aggiungendo i caratteri "cd"
+# - leggi il primo e l'ultimo valore in x
+# - rimuovi l'ultimo valore in x (tramite indice)
+# - aggiungi ad x l'array ["e", "f", ["g", "h"]]
+# - conta il numero di elementi in x
+# - ordina gli elementi in x in ordine alfabetico ascendente
+# - ordina gli elementi in x in ordine alfabetico discendente
+# - rovescia x
+# - pulisci x (clear)
